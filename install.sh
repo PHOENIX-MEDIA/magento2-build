a4c5311e7781fb3707153db7e335ab471ac834ed
Björn Kraus <bjoern.kraus@phoenix-media.eu>
2022-06-24 18:33:25 +0200

    Squashed commit of the following:
    commit 1a501caabdb4bb3d12a6e2a1e8a9bc79be238640
    Author: PhoenixPM - BK <bjoern.kraus@phoenix-media.eu>
    Date:   Fri Jun 24 16:36:10 2022 +0200
    
        fix Github Actions

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+MAGENTO_VERSION=2.4.4
+
 echo "Installing latest Magento2 OpenSource..."
-composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition src
+composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=${MAGENTO_VERSION} src
 
 echo "Installing CI/CD packages..."
-cd src; composer require phoenix-media/magento2-cloud-build --no-update; composer update; cd ..
+cd src
+composer require phoenix-media/magento2-cloud-build --no-update
+composer update
+vendor/bin/px-cloud-build-install
+cd ..
