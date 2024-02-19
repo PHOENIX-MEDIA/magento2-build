4da3ce6e39fa18f37bdd1826f3c7a11c5f601c9f
Björn Kraus <bjoern.kraus@phoenix-media.eu>
2024-02-19 17:06:18 +0100

    update php 8.2

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ echo "Cleaning up existing files..."
 rm -Rf .dockerignore .gitignore Dockerfile bin docker docker-compose.yml helm src
 
 echo "Installing source files..."
-docker run -t --rm -e MAGENTO_VERSION=${MAGENTO_VERSION} -v $(pwd):/var/www/html -v $(pwd)/.composer:/root/.composer phoenixmedia/nginx-php:8.1 bash -c "./install.sh"
+docker run -t --rm -e MAGENTO_VERSION=${MAGENTO_VERSION} -v $(pwd):/var/www/html -v $(pwd)/.composer:/root/.composer phoenixmedia/nginx-php:8.2 bash -c "./install.sh"
